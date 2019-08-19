9c772bdfbce3663e20471def19098b0d8593a34a
tim <tim.huang61@gmail.com>
2019-08-19 13:45:02 +0800

    pass Lab00 index

--- UnitTests/Lab00Tests.cs
+++ UnitTests/Lab00Tests.cs
@@ -1,7 +1,19 @@
+using AspNetCoreTest201908.Api.Lab00;
+using AspNetCoreTest201908.Model;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
 namespace UnitTests
 {
     public class Lab00Tests
     {
-        
+        [Fact]
+        public void Test_Index_Lab00Controller()
+        {
+            var lab00Controller = new Lab00Controller();
+            var actionResult = lab00Controller.Index().As<OkObjectResult>();
+            actionResult.Value.As<AuthResult>().IsAuth.Should().BeTrue();
+        }
     }
 }
