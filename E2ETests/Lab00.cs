ac26aa309831d815c677663c608a45428fb76f4b
tim <tim.huang61@gmail.com>
2019-08-27 13:55:03 +0800

    refactor TestBase

--- E2ETests/Lab00.cs
+++ E2ETests/Lab00.cs
@@ -10,11 +10,11 @@ namespace E2ETests
 {
     // IClassFixture 類似setup 
     // WebApplicationFactory : 建立內建web application
-    public class Lab00 : IClassFixture<WebApplicationFactory<Startup>>
+    public class Lab00 : TestBase
     {
         private readonly WebApplicationFactory<Startup> _factory;
 
-        public Lab00(WebApplicationFactory<Startup> factory)
+        public Lab00(WebApplicationFactory<Startup> factory) : base(factory)
         {
             _factory = factory;
         }
