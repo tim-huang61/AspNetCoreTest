3bb3702d751d870966eceafcbc1db9f80f908c0d
tim <tim.huang61@gmail.com>
2019-08-27 14:15:12 +0800

    build host

--- E2ETests/TestBase.cs
+++ E2ETests/TestBase.cs
@@ -1,6 +1,9 @@
 using System.Net.Http;
 using AspNetCoreTest201908;
+using AspNetCoreTest201908.Entity;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace E2ETests
@@ -8,15 +11,25 @@ namespace E2ETests
     public class TestBase : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly WebApplicationFactory<Startup> _factory;
+        protected WebApplicationFactory<Startup> AppWebHost;
 
         protected TestBase(WebApplicationFactory<Startup> factory)
         {
             _factory = factory;
         }
 
-        public HttpClient CreateHttpClient()
+        protected HttpClient CreateHttpClient()
         {
-            return _factory.CreateClient();
+            AppWebHost = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(config=>
+                {
+                    config.AddDbContext<AppDbContext>(
+                        option => option.UseInMemoryDatabase("memory"));
+                });
+            });
+
+            return AppWebHost.CreateClient();
         }
     }
 }
